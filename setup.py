1a5512bd7aef9d3a3f5e02c2447fa9ad5c4b8408
DmitryMurashov <dmtr.murashov@gmail.com>
2023-09-23 15:47:50 +0300

    [:setup] Build | "TIRED" dependency

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="fastgrpah",
+    packages=[
+        "fastgraph",
+    ],
+    include_package_data=True,
+    license="MIT",
+    description="Fast graph drawing and editing",
+    long_description="",
+    author="Dmitry Murashov",
+    setup_requires=["wheel"],
+    install_requires=[
+    	"tired @ git+https://github.com/damurashov/tired.git@master#egg=tired",
+    ],
+    dependency_links=[
+    	"",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Inependent",
+    ],
+    python_requires=">=3.7",
+    version="0.8.0",
+)
+
