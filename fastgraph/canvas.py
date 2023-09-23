2cf72e533355dcc38e04235710149fc7353cb5a8
DmitryMurashov <dmtr.murashov@gmail.com>
2023-09-23 16:30:37 +0300

    [fastgraph:canvas] Impl | Canvas with simple node drawing functionality

---
+++ fastgraph/canvas.py
@@ -0,0 +1,77 @@
+import fastgraph.logging
+import tkinter
+import tkinter.ttk
+import unittest
+
+
+_LOG_CONTEXT = "canvas"
+
+
+class _Tkinter:
+    MOUSE_BUTTON_LEFT = "<Button-1>"
+    MOUSE_BUTTON_LEFT_RELEASED = "<ButtonRelease-1>"
+    COLOR_BLACK = "black"
+
+
+class _CanvasMode:
+    """
+    See `Canvas
+    """
+    VIEWING = 0
+    DRAWING = 1
+
+
+class _CanvasNodeProperties:
+    size_pixels = 20
+    color = _Tkinter.COLOR_BLACK
+
+
+class Canvas(tkinter.Canvas):
+    _LOG_CONTEXT = f"fastgraph.{_LOG_CONTEXT}.Canvas"
+
+    def __init__(self, *args, **kwargs):
+        tkinter.Canvas.__init__(self, *args, **kwargs)
+        # Set default mode
+        self._mode = _CanvasMode.VIEWING
+
+        # Set default node properties
+        self._node_properties = _CanvasNodeProperties()
+
+        # Bind left mouse click
+        self.bind(_Tkinter.MOUSE_BUTTON_LEFT, self.on_left_button_clicked_canvas)
+
+    def set_drawing_mode(self):
+        """
+        The canvas gets switched into drawing mode: a user may draw nodes
+        """
+        self._mode = _CanvasMode.DRAWING
+
+    def update_render(self):
+        parent = self.winfo_parent()
+
+        if parent is None:
+            fastgraph.logging.error(Canvas._LOG_CONTEXT, f"update_render: cannot have `None` parent, raising exception")
+
+            raise ValueError
+
+        parent.update()
+
+    def add_node_at(self, x, y):
+        node_bounding_rectangle = ((x, y), (x + self._node_properties.size_pixels, y + self._node_properties.size_pixels))
+        node = self.create_oval(node_bounding_rectangle, fill=self._node_properties.color)
+
+    def on_left_button_clicked_canvas(self, event):
+        fastgraph.logging.debug(Canvas._LOG_CONTEXT, f"got event {event}")
+
+        if self._mode == _CanvasMode.DRAWING:
+            self.add_node_at(event.x, event.y)
+
+
+class InteractiveTest(unittest.TestCase):
+
+    def test_use_canvas(self):
+        root = tkinter.Tk()
+        canvas = Canvas(root, width=400, height=300)
+        canvas.pack(fill=tkinter.BOTH, expand=True)
+        canvas.set_drawing_mode()
+        root.mainloop()
