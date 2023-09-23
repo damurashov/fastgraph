f1cd05461ced96430498b4d3596e2d3591da6db4
DmitryMurashov <dmtr.murashov@gmail.com>
2023-09-23 16:43:41 +0300

    [fastgraph:logging] Ref | Hid boilerplate under a wrapping function

--- fastgraph/logging.py
+++ fastgraph/logging.py
@@ -5,11 +5,18 @@ import tired.datetime
 _LOG_SECTION_DELIMETER = "-"
 
 
-def debug(context, message):
-    print("D", _LOG_SECTION_DELIMETER, f"[{tired.datetime.get_today_time_seconds_string()}]", context, _LOG_SECTION_DELIMETER,
+def _log_impl(context, message, level):
+    print(level, _LOG_SECTION_DELIMETER, f"[{tired.datetime.get_today_time_seconds_string()}]", context, _LOG_SECTION_DELIMETER,
        message)
+
+
+def debug(context, message):
+    _log_impl(context, message, "D")
 
 
 def error(context, message):
-    print("E", _LOG_SECTION_DELIMETER, f"[{tired.datetime.get_today_time_seconds_string()}]", context, _LOG_SECTION_DELIMETER,
-       message)
+    _log_impl(context, message, "E")
+
+
+def info(context, message):
+    _log_impl(context, message, "I")
